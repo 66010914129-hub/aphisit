2191c468cc1b6cf0855a9f8a086d4889dade867c
66010914129-hub <66010914129@msu.ac.th>
2026-02-04 15:45:10 +0700

    Add files via upload

---
+++ f/a.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>อภิสิทธิ์ สีโยเฮียง</title>
+</head>
+
+<body>
+<h1>อภิสิทธิ์ สีโยเฮียง</h1>
+
+
+<script>
+        document.write("คณะการบัญชีการจัดการ")
+
+</script>
+<br>
+<?php
+  echo"Web programming<br>";
+  echo"Mahasarakham University<br>";
+  print"Aphisit Seeyohaeng<br>";
+ 
+  $name="อภิสิทธิ์ สีโยเฮียง";
+  $age= 22;
+  $Name="อภิสิทธิ์ สีโย";
+  
+  
+  //echo gettype($age);
+  var_dump($age);
+ 
+  echo"<hr>";
+  echo $name."<br>";
+  echo $Name."<hr>";
+  
+  $a=10;
+  $b=5;
+  $c=2;
+  
+  $x=($a+$b)*$c;
+  echo $x;
+ 
+?>
+</body>
+</html>
---
+++ f/d.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>อภิสิทธิ์ สีโยเฮียง</title>
+</head>
+
+<body>
+<h1>อภิสิทธิ์ สีโยเฮียง</h1>
+<?php
+for($i=1;$i<10;$i++){
+	echo"$i:อภิสิทธิ์ สีโยเฮียง<br>";
+	echo"<img src='1.jpg'width='250'><hr>";
+}
+?>
+</body>
+</html>
