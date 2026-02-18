e1ea3dec0d3911d5c9b780defa8198b4b98ca2ba
66010914129-hub <66010914129@msu.ac.th>
2026-02-18 16:42:19 +0700

    dada

---
+++ k/2.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="th">
+<head>
+<meta charset="utf-8">
+<title>อภิสิทธิ์ สีโยเฮียง</title>
+</head>
+
+<body>
+<h1>งานj</h1>
+<img src="1.jpen " alt="รูปภาพ" width="300">
+</body>
+</html>
