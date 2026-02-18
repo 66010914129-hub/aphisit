8f4a51010e33d6c58c3dfb29c163e79e1f36dc72
66010914129-hub <66010914129@msu.ac.th>
2026-02-18 16:17:44 +0700

    adadad

--- j/1.php
+++ j/1.php
@@ -1,12 +1,12 @@
 <!doctype html>
-<html>
+<html lang="th">
 <head>
 <meta charset="utf-8">
-<title>อภิสิทธฺิ์ สีโยเฮียง</ไตเติ้ล>
+<title>อภิสิทธิ์ สีโยเฮียง</title>
 </head>
 
 <body>
-<h1>งานj        </h1>
-<img src="1.png" alt="img"width="300">
+<h1>งาน</h1>
+<img src="1.png" alt="รูปภาพ" width="300">
 </body>
 </html>
